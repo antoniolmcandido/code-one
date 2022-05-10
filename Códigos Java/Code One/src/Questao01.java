2fd688ec5bc6b3488df812ab578a88325988251e
Candido <antoniolmcandido@gmail.com>
2022-05-10 18:37:41 -0300

    Questao01 Java

--- Códigos Java/Code One/src/Questao01.java
+++ Códigos Java/Code One/src/Questao01.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Questao01 {
     public static void main(String[] args) throws Exception {
-        int numero = 150;
-        
-        System.out.println((float) numero/7);
-        
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Digite um número entre 1 e 10: ");
+            int numero = entrada.nextInt();
+
+            for(int i = 1; i <= 10; i++){
+                System.out.println("Tabuada de " + numero +": " +numero+ " * " +i+" = " +(numero*i));
+            }
+        }        
     }
-}
