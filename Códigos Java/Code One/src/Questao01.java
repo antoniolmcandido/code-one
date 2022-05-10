ac8a49f7e87250802528b1024ec9cc5ce1159018
Candido <antoniolmcandido.com>
2022-05-10 16:22:16 -0300

    atualização de arquivos

---
+++ Códigos Java/Code One/src/Questao01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Questao01 {
+    public static void main(String[] args) throws Exception {
+        int numero = 150;
+        
+        System.out.println((float) numero/7);
+        
+    }
+}
